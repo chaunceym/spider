1b3bade37b374536609c1f41c895d292bf6aa32d
chaunceym <18322273773@163.com>
2020-05-21 12:47:49 +0800

    完成 jwt token 存储

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,26 @@
 const express = require('express')
 const router = express.Router()
+const JWT = require('jsonwebtoken')
 
 const userRouter = require('./users')
-const users = []
 /* GET home page. */
 
 router.get('/login', (req, res, next) => {
   const {username} = req.query
-  req.session.user = {username}
-  res.send('done')
+  const user = { username}
+  const token = JWT.sign(user,'asdffniangsddngo')
+  res.send(token)
 })
 
 router.get('/hello', (req, res, next) => {
-  console.log(req.session)
-  const {username} = req.session.user
-  res.send(`<h1>hello: ${username}</h1>`)
+  const auth = req.get('Authorization')
+  if(!auth) return res.send('no auth')
+  if(!auth.indexOf('Bearer ') === -1) res.send('no auth')
+  const token = auth.split('Bearer ')[1]
+  const user = JWT.verify(token, 'asdffniangsddngo')
+  res.send(user)
 })
+
 
 router.use('/user', userRouter)
 
